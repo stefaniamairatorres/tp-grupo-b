43cbee4629b6f20d9866bc4292510c9765449c16
Maira <stefaniamairatorres@gmail.com>
2025-11-10 19:21:16 -0300

    chore: eliminar configuración de Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  
+  // ¡¡¡ESTO ES LO NUEVO!!!
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false, 
+        rewrite: (path) => path.replace(/^\/api/, '/api'), 
+      },
+    },
+    port: 5173,
+    open: true,
+  },
+});
